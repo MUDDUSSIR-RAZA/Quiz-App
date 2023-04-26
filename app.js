d3cd9781660ac48894e24a7a46f36f94a9154e58
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-04-26 22:44:19 +0500

    add comments on js file

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
+// Selecting the elements from the DOM
 const quizzNameContainer = document.querySelector(".quizzNamecontainer");
 const quizzQuestionsContainer = document.querySelector(".quizzQuestionsContainer");
 const resultContainer = document.querySelector(".resultContainer");
 
-
+// Defining the Quiz class
 class Quiz {
     constructor(questions) {
+        // Randomizing the order of the questions
         this.questions = Quiz.shuffleArray(questions.questions);
         this.currentQuestionIndex = 0;
         this.score = 0;
         this.displayQuestions();
     }
 
+    // Method to display the questions
     displayQuestions() {
+        // Clearing the previous question and answers
         quizzQuestionsContainer.innerHTML = "";
+        // Selecting the current question
         const currentObject = this.questions;
         const currentQuestion = currentObject[this.currentQuestionIndex];
         const ansDiv = document.createElement("div");
         ansDiv.className = ("answers");
+
+        // Creating HTML for the current question and its options
         quizzQuestionsContainer.innerHTML = `
-        <div class="questionNumber">
+      <div class="questionNumber">
         <h3>
-        QUESTION ${this.currentQuestionIndex + 1}
+          QUESTION ${this.currentQuestionIndex + 1}
         </h3>
-        </div>
-        <div class="question">
+      </div>
+      <div class="question">
         <h4>
-        ${currentQuestion.question}
+          ${currentQuestion.question}
         </h4>
-        </div>
-        `;
+      </div>
+    `;
+
+        // Shuffling the options and displaying them as buttons
         const shufelledOptions = Quiz.shuffleArray(currentQuestion.options);
         shufelledOptions.forEach((questions) => {
             const button = document.createElement("button");
@@ -39,6 +48,7 @@ class Quiz {
         });
     }
 
+    // Method to check the selected answer and update score
     checkAnswer(event) {
         const selectAnswer = event.target.textContent;
         const currentObject = this.questions;
@@ -49,28 +59,31 @@ class Quiz {
 
         this.currentQuestionIndex++;
 
+        // If there are more questions, display the next question
         if (this.currentQuestionIndex < currentObject.length) {
             this.displayQuestions();
         } else {
+            // Otherwise, show the quiz result
             this.showResult();
         }
     }
 
+    // Method to display the final result
     showResult() {
         quizzQuestionsContainer.style.display = "none";
         resultContainer.style.display = "block";
         resultContainer.innerHTML = `
-        <div class="resultHeading">
-                <h2>
-                    Result
-                </h2>
-            </div>
-            <div class="result">
-                <p>
-                    you Scored ${this.score} out of 15 Questions.
-                </p>
-            </div>
-        `;
+      <div class="resultHeading">
+        <h2>
+          Result
+        </h2>
+      </div>
+      <div class="result">
+        <p>
+          you Scored ${this.score} out of 15 Questions.
+        </p>
+      </div>
+    `;
         const btnDiv = document.createElement("div");
         btnDiv.className = "reloadQuiz";
         const btn = document.createElement("button");
@@ -78,36 +91,43 @@ class Quiz {
         btn.innerText = "Reload Quiz";
         btnDiv.appendChild(btn);
         resultContainer.appendChild(btnDiv);
-        btn.addEventListener("click" , () => {
+
+        // Reload the quiz when the button is clicked
+        btn.addEventListener("click", () => {
             quizzNameContainer.style.display = "flex";
             quizzQuestionsContainer.style.display = "none";
             resultContainer.style.display = "none";
         });
     }
 
-     static shuffleArray(arr) {
-    return [...arr].sort(() => Math.random() - 0.5);
-  }
+    // Static method to shuffle an array
+    static shuffleArray(arr) {
+        return [...arr].sort(() => Math.random() - 0.5);
+    }
 }
 
 
 
+// Function to load a quiz
 const loadQuizz = (questions) => {
     const quiz = new Quiz(questions);
-    quizzNameContainer.style.display = "none";
-    quizzQuestionsContainer.style.display = "block";
+    quizzNameContainer.style.display = "none"; // Hide quiz name container
+    quizzQuestionsContainer.style.display = "block"; // Show quiz questions container
 }
 
+// Function to load all quizzes
 const loadAllQuizzes = async () => {
     const response = await fetch("./quiz.json");
     const quizzes = await response.json();
 
+    // Sort quizzes in alphabetical order by quiz title
     quizzes.sort((a, b) => {
         if (a.quizTitle < b.quizTitle) return -1;
         if (a.quizTitle > b.quizTitle) return 1;
         return 0;
     });
 
+    // Loop through each quiz and create a clickable div for it
     quizzes.forEach((quiz) => {
         const div = document.createElement("div");
         div.className = ("quizzName");
@@ -117,4 +137,5 @@ const loadAllQuizzes = async () => {
     })
 };
 
+// Call the function to load all quizzes when the page loads
 loadAllQuizzes();
